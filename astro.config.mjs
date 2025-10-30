861c610aa5c384916c6d55c523a84a6c45bf3d28
Anthony Mace <anthony@anthonymace.com>
2025-10-30 15:12:51 +1300

    add preact for islands

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://tedx-akl-astro-test.netlify.app/"
+  site: "https://tedx-akl-astro-test.netlify.app/",
+  integrations: [preact()]
 });
